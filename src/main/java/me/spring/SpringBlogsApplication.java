b84b58f5990ae8c689f175b34e1f65273f8585d4
flwkemd <flwkemd@naver.com>
2017-11-13 23:40:14 +0100

    initial commit

---
+++ src/main/java/me/spring/SpringBlogsApplication.java
@@ -0,0 +1,21 @@
+package me.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringBlogsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBlogsApplication.class, args);
+	}
+	
+	@GetMapping
+	public String hello() {
+		return "hello";
+	}
+	
+}
